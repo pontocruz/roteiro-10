b63c25f4159ee6ceb6d03c0ed6bfc637b625aa3b
paulo <paulo.go.disc@gmail.com>
2025-07-21 03:27:22 -0300

    pomelo

--- ProjetoAplicativo/Program.cs
+++ ProjetoAplicativo/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetoAplicativo.Data;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var conexaoRoteiro = builder.Configuration.GetConnectionString("DefaultConnection"); 
+var versaoRoteiro = ServerVersion.AutoDetect(conexaoRoteiro);
+builder.Services.AddDbContext<ModeloEntidades>(options => options.UseMySql(conexaoRoteiro, versaoRoteiro));
 var app = builder.Build();
 if (app.Environment.IsDevelopment()) { app.UseSwagger(); app.UseSwaggerUI(); }
 app.UseHttpsRedirection();
